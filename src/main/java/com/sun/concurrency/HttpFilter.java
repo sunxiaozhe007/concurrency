83da9bb41d320a4a6b75055b2b7361c459c4c7b8
sunxiaozhe007 <985160269@qq.com>
2018-10-22 22:13:06 +0800

    过滤器

---
+++ src/main/java/com/sun/concurrency/HttpFilter.java
@@ -0,0 +1,37 @@
+package com.sun.concurrency;
+
+import com.sun.concurrency.threadlocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @program concurrency
+ * @author: sunxiaozhe
+ * @create: 2018/10/22 17:06
+ */
+@Slf4j
+public class HttpFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        log.info("do filter,{},{}",Thread.currentThread().getId(),request.getServletPath());
+        RequestHolder.add(Thread.currentThread().getId());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
