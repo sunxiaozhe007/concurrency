f29dde877e957bb313f5e1d6d50fd7bdd972695d
sunxiaozhe007 <985160269@qq.com>
2018-10-22 22:13:56 +0800

    threadlocal测试

---
+++ src/main/java/com/sun/concurrency/HttpInterceptor.java
@@ -0,0 +1,30 @@
+package com.sun.concurrency;
+
+import com.sun.concurrency.threadlocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @program concurrency
+ * @author: sunxiaozhe
+ * @create: 2018/10/22 17:15
+ */
+@Slf4j
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("preHandle");
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        RequestHolder.remove();
+        log.info("afterCompletion");
+        return;
+    }
+}
