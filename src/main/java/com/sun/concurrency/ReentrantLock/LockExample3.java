055123a3fca7e8b6955b938d4ebb5dc7de35206b
sunxiaozhe007 <985160269@qq.com>
2018-10-31 22:33:37 +0800

    ReentrantLock

---
+++ src/main/java/com/sun/concurrency/ReentrantLock/LockExample3.java
@@ -0,0 +1,54 @@
+package com.sun.concurrency.ReentrantLock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.xml.crypto.Data;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @program concurrency
+ * @author: sunxiaozhe
+ * @create: 2018/10/23 19:15
+ */
+@Slf4j
+public class LockExample3 {
+
+    private final Map<String, Data> map = new TreeMap<>();
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private final Lock readLock = lock.readLock();
+
+    private final Lock writeLock = lock.writeLock();
+
+    public Data get(String key){
+        readLock.lock();
+        try {
+            return map.get(key);
+        }finally {
+            readLock.unlock();
+        }
+    }
+
+    public Set<String> getAllKeys(){
+        readLock.lock();
+        try {
+            return map.keySet();
+        }finally {
+            readLock.unlock();
+        }
+    }
+
+    public Data put(String key,Data value){
+        writeLock.lock();
+        try {
+            return map.put(key,value);
+        }finally {
+            readLock.unlock();
+        }
+    }
+}
---
+++ src/main/java/com/sun/concurrency/ReentrantLock/LockExample4.java
@@ -0,0 +1,60 @@
+package com.sun.concurrency.ReentrantLock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * @program concurrency
+ * @author: sunxiaozhe
+ * @create: 2018/10/23 20:18
+ */
+@Slf4j
+public class LockExample4 {
+    //请求总数
+    public static int clientTotal = 5000;
+
+    //同时并发执行的线程总数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private final static StampedLock lock = new StampedLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i = 0; i < clientTotal; i++){
+            executorService.execute(() ->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (InterruptedException e) {
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",count);
+    }
+
+    private static void add(){
+        long stamp = lock.writeLock();
+        try {
+            count++;
+        }finally {
+            lock.unlock(stamp);
+        }
+    }
+}
